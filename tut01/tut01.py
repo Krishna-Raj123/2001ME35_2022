6dc010429c568f643f50625ae9659360a374d13c
krishna raj <kraj.krishna11@gmail.com>
2022-09-14 22:32:12 +0530

    reading the file

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,8 +1,17 @@
-from turtle import hideturtle
+def octact_identification(mod=5000):
+import pandas as pd
+df = pd.read_csv(r"C:\Users\hp\OneDrive\Documents\New folder\2001ME35_2022\tut01\octant_input.csv")
+x = df.shape[0]
+df.head(15)
 
 
-def factorial(int x):
-    
-hideturtle
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+mod=5000
+octact_identification(mod)
